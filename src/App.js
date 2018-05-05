ba4c5039190d1893aab87479a9304cbc889d1e00
Momit Rahman <momitrahman@gmail.com>
2018-05-05 14:10:35 +0600

    Add SearcBar input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Products from './containers/Products';
+import Header from './containers/Header';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Header />
         <Products />
       </React.Fragment>
     );
---
+++ src/containers/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from '../UI/SearchBar';
+
+class Header extends React.Component {
+  // state = {  }
+  render() {
+    return (
+      <React.Fragment>
+        <SearchBar />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Header;
